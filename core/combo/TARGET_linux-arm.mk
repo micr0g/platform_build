613de9e9e7f6fa72555858dea9b7dc955bda81a0
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2014-12-09 23:13:57 +0100

    targets O3 flags for arm and thumber targets
    Change-Id: Ia760dd20be004fc29b2783911e346a338e58c10f

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -67,7 +67,7 @@ $(combo_2nd_arch_prefix)TARGET_STRIP := $($(combo_2nd_arch_prefix)TARGET_TOOLS_P
 
 $(combo_2nd_arch_prefix)TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O2 \
+$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O3 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops     \
@@ -76,7 +76,7 @@ $(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O2 \
 
 # Modules can choose to compile some source as thumb.
 $(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS :=  -mthumb \
-                        -O2 \
+                        -O3 \
                         -fomit-frame-pointer \
                         -fno-strict-aliasing \
                         -ftree-vectorize \
